36c24d164ae5a8d7144e143babf067f6daa518af
Mihail Schiopu <schiopu.mihail96@gmail.com>
2023-10-17 16:38:00 +0300

    MVC

---
+++ server/controller/users.js
@@ -0,0 +1,51 @@
+const users = require('../models/users.json');
+
+const initialData = [...users];
+
+const getAllUsers = (req, res) => {
+  res.json(initialData);
+};
+
+const createUser = (req, res) => {
+  initialData.push(req.body);
+  res.json({ ...req.body });
+};
+
+const updateUser = (req, res) => {
+  const idx = initialData.findIndex((user) => user.id === req.body?.id);
+
+  if (idx !== -1) {
+    initialData[idx] = req.body;
+    res.json(req.body);
+  } else {
+    res.send('Error');
+  }
+};
+
+const deleteUser = (req, res) => {
+  const idx = initialData.findIndex((user) => user.id === req.body?.id);
+  console.log(req.body);
+  if (idx !== -1) {
+    delete initialData[idx];
+    res.send(`${req.body.id} was deleted`);
+  } else {
+    res.send('Error');
+  }
+};
+
+const getUserById = (req, res) => {
+  const user = initialData.find((user) => user.id === Number(req.params?.id));
+  if (user) {
+    res.json([user]);
+  } else {
+    res.send('Error');
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,47 +1,21 @@
-const users = require('./users.json');
 const express = require('express');
 const router = express.Router();
-
-const initialData = [...users];
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controller/users');
 
 router
   .route('/')
-  .get((req, res) => {
-    res.json(initialData);
-  })
-  .post((req, res) => {
-    initialData.push(req.body);
-    res.json({ ...req.body });
-  })
-  .put((req, res) => {
-    const idx = initialData.findIndex((user) => user.id === req.body?.id);
-
-    if (idx !== -1) {
-      initialData[idx] = req.body;
-      res.json(req.body);
-    } else {
-      res.send('Error');
-    }
-  })
-  .delete((req, res) => {
-    const idx = initialData.findIndex((user) => user.id === req.body?.id);
-    console.log(req.body);
-    if (idx !== -1) {
-      delete initialData[idx];
-      res.send(`${req.body.id} was deleted`);
-    } else {
-      res.send('Error');
-    }
-  });
+  .get(getAllUsers)
+  .post(createUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 // get by query params
-router.route('/:id').get((req, res) => {
-  const user = initialData.find((user) => user.id === Number(req.params?.id));
-  if (user) {
-    res.json([user]);
-  } else {
-    res.send('Error');
-  }
-});
+router.route('/:id').get(getUserById);
 
 module.exports = router;
